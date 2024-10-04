35a2a699d0ba36650ec0fe2c57a043c539f2812c
UserK <111083281+FatalUserK@users.noreply.github.com>
2024-10-05 00:48:50 +0100

    basically finished potion_starting lib

--- potion_starting/potion_starting.lua
+++ potion_starting/potion_starting.lua
@@ -7,22 +7,30 @@ dofile_once("data/scripts/items/init_potion.lua")
 local potion_material = "water"
 
 ---Current UTC time
-local UTC = {}
+UTC = {}
 UTC.year, UTC.month, UTC.day, UTC.hour, UTC.minute, UTC.second = GameGetDateAndTimeUTC()
 
 ---Current Local time (includes jussi and mammi bools)
-local LOCAL = {}
+LOCAL = {}
 LOCAL.year, LOCAL.month, LOCAL.day, LOCAL.hour, LOCAL.minute, LOCAL.second, LOCAL.jussi, LOCAL.mammi = GameGetDateAndTimeLocal()
 
 
+---This function combines tables so you can easily merge your custom tables into the vanilla ones
+function CombineTables(base, addition)
+	for i=1,#addition do
+		base[#base+1] = addition[i]
+	end
+	return base
+end
+
 ---Function that compares two tables to see if they are identical. Can be used to easily check between current date and desired date using tables. By Nahtan
-local function compare_tables(a, b) 
+function CompareTables(a, b) 
     if type(a) ~= type(b) then
         return false
     end
     if type(a) == "table" then
         for k, v in pairs(a) do
-            if not compare_tables(v, b[k]) then
+            if not CompareTables(v, b[k]) then
                 return false
             end
         end
@@ -32,26 +40,17 @@ local function compare_tables(a, b)
 end --awesomium function grabbed from nathan (i was incompetent so he made the function for me)
 
 ---This function just corrects the tables that lack probability fields. Probability will default to 10 for all material tables
-local function correct_tables()
-	for index, value in ipairs({starterpotions, magicpotions, fail_list}) do
+function CorrectTables()
+	for index, value in ipairs({starterpotions, magicpotions, failpotions}) do
 		for k, v in ipairs(value) do
-			if v.probability == nil then v.probability = 10 end
+			if v.probability == nil then v.probability = 10 print(v[1] .. " corrected.") end
 		end
 	end
 end
 
----This function combines tables so you can easily merge your custom tables into the vanilla ones
-local function combine_tables(base, addition)
-	for i=1,#addition do
-		base[#base+1] = addition[i]
-	end
-	return base
-end
-
-
 
 ---starter potions, 70% chance to pull from this table
-local starterpotions = {
+starterpotions = {
 	{	probability = 32.5, 	"water"			},
 	{	probability = 6.5, 		"mud"			},
 	{	probability = 6.5, 		"water_swamp"	},
@@ -62,7 +61,7 @@ local starterpotions = {
 }
 
 ---magic potions, 29% chance to pull from this table
-local magicpotions = {
+magicpotions = {
 	{	"acid"							},
 	{	"magic_liquid_polymorph"		},
 	{	"magic_liquid_random_polymorph"	},
@@ -72,22 +71,22 @@ local magicpotions = {
 }
 
 	-- 1/10,000,100 technically, lmao.
-local one_in_millions = { -- "key" must be from 1 to 100000
+one_in_millions = { -- "key" must be from 1 to 100000
 	{	key = 666, 	"urine"},
 	{	key = 79, 	"gold"}
 }
 
 ---Material outcomes if the "one-in-a-million" doesnt hit anything
-local failpotions = {
+failpotions = {
 	{	"slime"					},
 	{	"gunpowder_unstable"	}
 }
 
 ---Table of custom functions potion_a_materials runs through before returning the chosen material
 ---@type fun(outcome: string, r_value: number, r_value2: number, data: table)[]
-local functions = {
+functions = {
 	function()
-		if compare_tables({LOCAL.month, LOCAL.day}, {5, 1}) or compare_tables({LOCAL.month, LOCAL.day}, {4, 30}) and (Random( 0, 100 ) <= 20) then return "sima" end
+		if CompareTables({LOCAL.month, LOCAL.day}, {5, 1}) or CompareTables({LOCAL.month, LOCAL.day}, {4, 30}) and (Random( 0, 100 ) <= 20) then return "sima" end
 	end
 }
 
@@ -95,6 +94,9 @@ local functions = {
 ---@type fun(potion_material: string, data: table)[]
 local functions2 = {
 }
+
+
+
 
 ---Function that chooses the potion material
 local function potion_a_materials(outcome, r_value, r_value2, data) --Variables are available as inputs in case you want to run with forced RNG. data lets you pass extra data if you want
@@ -129,7 +131,9 @@ end
 function init( entity_id ) --mostly vanilla function
 	local x,y = EntityGetTransform( entity_id )
 	SetRandomSeed( x, y )
-	correct_tables() --add default probabilities
+	CorrectTables() --add default probabilities
+
+	TEST(100000) --1 billion took about 45 minutes for me to render so 100k-10mil should be good enough for regular tests (probs still super excessive tbh lmao)
 
 	local n_of_deaths = tonumber( StatsGlobalGetValue("death_count") )
 	if( n_of_deaths >= 1 ) then
@@ -144,4 +148,45 @@ function init( entity_id ) --mostly vanilla function
 	end
 
 	init_potion( entity_id, potion_material )
+end
+
+
+
+
+--Handy script for mass-generating potion outcomes
+function TEST(num)
+	local keys = {}
+	local _table = {}
+	
+	for i = 1, num do
+		if i % (num/100) == 0 then print(i/(num/100).."%") end --prints n% when n is a whole number (basically print current% without decimals)
+		local result = potion_a_materials() --simulate potion start
+		_table[result] = (_table[result] or 0) + 1 --add string to table, give it +1 every time its returned
+	end
+	
+	local t = {}
+	for k, v in pairs(_table) do
+	  table.insert(t, { key = k, value = v }) --sort function grabbed from Ribbit and Horscht after very painstakingly futile attempts to explain this to me
+	end
+	table.sort(t, function(a, b)
+	  return a.value > b.value
+	end)
+
+	for i, j in ipairs(t) do --print table
+		local material_name = j.key
+		local material_amount = j.value
+		local material_percent = j.value/(num/100)
+		local material_percent_rounded = math.floor(((j.value/(num/100)) + 0.5) * 10) * .1
+
+		for l = 1, 36 - string.len(material_name) do
+			material_amount = ' ' .. material_amount
+		end
+		for l = 1, 12 - string.len(tostring(material_amount)) do
+			material_percent = ' ' .. material_percent
+		end
+		for l = 1, 16 - string.len(material_percent) do
+			material_percent_rounded = ' ' .. material_percent_rounded
+		end
+		print(material_name .. " = " .. material_amount .. ", " .. material_percent .. "%, " .. material_percent_rounded .. "%")
+	end
 end
